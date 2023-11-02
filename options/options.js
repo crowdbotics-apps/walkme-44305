e30113a191afae5755099f2d4865cbb36cade228
Crowdbotics <team@crowdbotics.com>
2023-11-02 21:03:44 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "walkme-44305",
+  url: "https://walkme-44305.botics.co",
+  api: "https://walkme-44305.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
